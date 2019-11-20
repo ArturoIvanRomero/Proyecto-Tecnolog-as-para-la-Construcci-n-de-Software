23cdec5f2446dabacbeb53fa1efbb1b7d6bd8c7f
JosueAcevedo <48764151+JosueAcevedo@users.noreply.github.com>
2019-11-19 19:23:37 -0600

    Add files via upload

---
+++ SerpientesYEscaleras/SerpientesYEscaleras/SerpientesYEscalerasTests/MainWindowTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerpientesYEscaleras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerpientesYEscaleras.Tests {
+  [TestClass()]
+  public class MainWindowTests {
+
+    MainWindow main = new MainWindow();
+    Boolean expectedFalse = false;
+    Boolean expectedTrue = true;
+
+    [TestMethod()]
+    public void ValidarDatosCompletosTest1() {
+      Boolean actual = main.ValidarDatosCompletos("", "pala");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarDatosCompletosTest2() {
+      Boolean actual = main.ValidarDatosCompletos("nomre", "pala");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarNombreUsuarioTest1() {
+      Boolean actual = main.ValidarNombreUsuario("nomre");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+
+    [TestMethod()]
+    public void ValidarNombreUsuarioTest2() {
+      Boolean actual = main.ValidarNombreUsuario("nomr*e");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ComputeSha256HashTest() {
+      String actual = main.ComputeSha256Hash("1234567a");
+      String expected = "47ef20207489b775fa4cdcac3c394b517ab22d7460237ae3df1ac0e8963699d6";
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
---
+++ SerpientesYEscaleras/SerpientesYEscaleras/SerpientesYEscalerasTests/RegistrarseTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerpientesYEscaleras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerpientesYEscaleras.Tests {
+  [TestClass()]
+  public class RegistrarseTests {
+    Registrarse registro = new Registrarse();
+    Boolean expectedFalse = false;
+    Boolean expectedTrue = true;
+
+    [TestMethod()]
+    public void ValidarDatosCompletosTest() {
+      Boolean actual = registro.ValidarDatosCompletos("a", "a", "a", "a");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarDatosCompletosTest2() {
+      Boolean actual = registro.ValidarDatosCompletos("c", "", "a", "a");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarDatosCompletosTest3() {
+      Boolean actual = registro.ValidarDatosCompletos("x", "a", "", "a");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarDatosCompletosTest4() {
+      Boolean actual = registro.ValidarDatosCompletos("x", "a", "x", "");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarNombreUsuarioTest1() {
+      Boolean actual = registro.ValidarNombreUsuario("gujfyfcghbj");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarNombreUsuarioTest2() {
+      Boolean actual = registro.ValidarNombreUsuario("ujhgjhb.");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarContraseniaTest() {
+      Boolean actual = registro.ValidarContrasenia("ujhgb");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarContraseniaTest2() {
+      Boolean actual = registro.ValidarContrasenia("ujhgbjhgfdfg");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarContraseniaTest3() {
+      Boolean actual = registro.ValidarContrasenia("123456789");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarContraseniaTest4() {
+      Boolean actual = registro.ValidarContrasenia("123456789kjhg");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void VerificarContraseniaTest() {
+      Boolean actual = registro.VerificarContrasenia("123456789kjhg", "123456789kjhg");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void VerificarContraseniaTest2() {
+      Boolean actual = registro.VerificarContrasenia("123456789kjhg", "123456789kjjhg");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarCorreoElectronicoTest() {
+      Boolean actual = registro.ValidarCorreoElectronico("123456789kjhg");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarCorreoElectronicoTest2() {
+      Boolean actual = registro.ValidarCorreoElectronico("12345678@9kjhg");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarCorreoElectronicoTest3() {
+      Boolean actual = registro.ValidarCorreoElectronico("12345678@.9kjhg");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void ValidarCorreoElectronicoTest4() {
+      Boolean actual = registro.ValidarCorreoElectronico("josue@hotmail.9kjhg");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void VerificarNombreUsuarioTest() {
+      Boolean actual = registro.VerificarNombreUsuario("Arturo2");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void VerificarNombreUsuarioTest2() {
+      Boolean actual = registro.VerificarNombreUsuario("Arturo22");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+
+    [TestMethod()]
+    public void VerificarCorreoElectronicoTest() {
+      Boolean actual = registro.VerificarCorreoElectronico("Arturo2@hotmail.com");
+      Assert.AreEqual(expectedFalse, actual);
+    }
+
+    [TestMethod()]
+    public void VerificarCorreoElectronicoTest2() {
+      Boolean actual = registro.VerificarCorreoElectronico("josuefresh@hotmail.com");
+      Assert.AreEqual(expectedTrue, actual);
+    }
+  }
+}
